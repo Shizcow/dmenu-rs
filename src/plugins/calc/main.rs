e6dc850954858b5344d6e061f8d6c3534499813a
Shizcow <pohl.devin@gmail.com>
2020-06-22 14:18:15 -0600

    Found a calc lib I'm happy with

--- src/plugins/calc/main.rs
+++ src/plugins/calc/main.rs
@@ -3,15 +3,21 @@ use overrider::*;
 use crate::drw::Drw;
 use crate::item::Item;
 
-// deps: sagemath, python-babel
+use rink::*;
+
 
 #[override_flag(flag = calc)]
 impl Drw {
     pub fn gen_matches(&mut self) -> Result<Vec<Item>, String> {
-	let out = match Item::new(self.input.clone(), false, self) {
-	    Ok(item) => item,
-	    Err(err) => return Err(err),
-	};
-	Ok(vec![out])
+	
+	let mut ctx = load().unwrap();
+	if let Ok(evaluated) = one_line(&mut ctx, &self.input) {
+	    Ok(vec![match Item::new(evaluated, false, self) {
+		Ok(item) => item,
+		Err(err) => return Err(err),
+	    }])
+	} else {
+	    Ok(vec![])
+	}
     }
 }
