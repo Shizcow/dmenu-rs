c3fec78e019a7edbe3346d2d1a142ecea37b464f
_ <a@b.co>
2020-07-22 17:56:13 +0530

    Autoselect plugin
    Add autoselect flag which would autoselect if there is only one current
    match

---
+++ src/plugins/autoselect/main.rs
@@ -0,0 +1,25 @@
+use overrider::*;
+
+#[allow(unused_imports)]
+use crate::clapflags::CLAP_FLAGS;
+
+use crate::drw::Drw;
+use crate::item::Item;
+use crate::result::*;
+
+
+
+#[override_flag( flag=autoselect )]
+impl Drw {
+    pub fn postprocess_matches( &mut self, current_matches: Vec<Item> ) -> CompResult<Vec<Item>> {
+		if	
+			current_matches.len() == 1 
+		{
+			Err( Die::Stdout( current_matches[ 0 ].text.to_string() ) )
+		}
+		else
+		{
+			Ok( current_matches )
+		}
+    }
+}
