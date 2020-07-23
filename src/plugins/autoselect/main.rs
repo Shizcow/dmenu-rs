cdf9c96169d5409f2d6b4f72ce2dcbd24e0457ff
Shizcow <pohl.devin@gmail.com>
2020-07-22 19:59:28 -0600

    Tweaked postprocess_matches in autoselect:
    - Use self.dispose instead of Die::Stdout to print item contents
      - This doesn't change anything normally, but if a plugin overrode dispose, this change would emit correct behavior
    - Formatting tweaks because I like it that way

--- src/plugins/autoselect/main.rs
+++ src/plugins/autoselect/main.rs
@@ -4,18 +4,14 @@ use crate::drw::Drw;
 use crate::item::Item;
 use crate::result::*;
 
-
-#[override_flag( flag=autoselect )]
+#[override_flag(flag = autoselect)]
 impl Drw {
-    pub fn postprocess_matches( &mut self, current_matches: Vec<Item> ) -> CompResult<Vec<Item>> {
-	if	
-	    current_matches.len() == 1 
-	{
-	    Err( Die::Stdout( current_matches[ 0 ].text.to_string() ) )
-	}
-	else
-	{
-	    Ok( current_matches )
+    pub fn postprocess_matches(&mut self, mut current_matches: Vec<Item>) -> CompResult<Vec<Item>> {
+	if current_matches.len() == 1 {
+	    self.dispose(current_matches.swap_remove(0).text, true)?;
+	    Err(Die::Stdout("".to_owned()))
+	} else {
+	    Ok(current_matches)
 	}
     }
 }
