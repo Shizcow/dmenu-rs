7ed84424339231951ea12635f0e9241fd359485b
Shizcow <pohl.devin@gmail.com>
2020-11-28 19:15:56 -0700

    Made some edits to the lookup plugin. Moved the engines definition to a seperate file for clarity, changed query substitution for versatility

--- src/plugins/lookup/main.rs
+++ src/plugins/lookup/main.rs
@@ -5,41 +5,22 @@ use crate::clapflags::CLAP_FLAGS;
 use crate::config::ConfigDefault;
 use crate::drw::Drw;
 use crate::result::*;
+use itertools::Itertools;
 
-use once_cell::sync::Lazy;
-use std::{collections::HashMap, sync::Mutex};
-
-// the user can simply define here more pairs of engine => url
-static ENGINES: Lazy<Mutex<HashMap<String, &'static str>>> = Lazy::new(|| {
-    let mut m = HashMap::new();
-    m.insert("crates".to_string(), "https://crates.io/crates/");
-    m.insert("github".to_string(), "https://github.com/search?q=");
-    m.insert("rust".to_string(), "https://doc.rust-lang.org/std/?search=");
-    m.insert(
-        "archwiki".to_string(),
-        "https://wiki.archlinux.org/index.php?search=",
-    );
-    m.insert("ddg".to_string(), "https://duckduckgo.com/");
-    m.insert(
-        "english".to_string(),
-        "https://www.merriam-webster.com/dictionary/",
-    );
-    m.insert("docs".to_string(), "https://docs.rs/");
-    Mutex::new(m)
-});
+mod engines;
+use engines::ENGINES;
 
 // Format engine as prompt
 // eg "ddg" -> "[Search ddg]"
 fn create_search_input(engine: &str) -> CompResult<String> {
-    let engines = ENGINES.lock().unwrap();
     // fail early if engine is wrong
-    match engines.get(&engine.to_string()) {
+    match ENGINES.get(engine) {
         Some(_) => Ok(format!("[Search {}]", engine)),
         None => {
             return Err(Die::Stderr(format!(
-                "Invalid search search engine {}. Valid options are: {:?}",
+                "Invalid search search engine {}. Valid options are: {}",
                 engine,
-                engines.keys()
+                ENGINES.keys().map(|e| format!("\"{}\"", e)).join(", ")
             )))
         }
     }
@@ -52,11 +33,8 @@ fn do_dispose(output: &str, prompt: &str) -> CompResult<()> {
     engine.pop();
 
     // just unwrap since the check was performed before
-    let search_prompt = format!(
-        "{}{}",
-        ENGINES.lock().unwrap().get(&engine).unwrap(),
-        output
-    );
+    let search_prompt = ENGINES.get(engine.as_str())
+	.unwrap().to_string().replace("%s", output);
 
     // TODO: consider user defined open command for cross-platform awareness
     Command::new("xdg-open")
